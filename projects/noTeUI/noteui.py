fc4ccfe10ffaf194708f02995b92ddda432ffc50
runtime-erop <egor.srvc.letovo@gmail.com>
2025-10-01 20:56:00 +0300

    added text interface, code polished, minor bugfix

--- projects/noTeUI/noteui.py
+++ projects/noTeUI/noteui.py
@@ -1,14 +1,13 @@
 ﻿def interface():
     while True:
-        print('''
-                 1. +n
-                 2. -n 
-                 3. /s
-                 4. *n
-                 5. /e
-                 ''')
-        a = input()
-        match a:
+        print('''What do you want me to do?
+                 1. Create a new note
+                 2. Delete your note
+                 3. Search for a note
+                 4. View notes
+                 5. Exit''')
+        act = input()
+        match act:
             case '1':
                 create()
             case '2':
@@ -19,38 +18,50 @@
                 view()
             case '5':
                 close()
+            case _:
+                print('err: invalid action')
+                continue
 
 def create():
-    note = input()
-    with open("notes.nt", "a") as f:
-        f.write(note + "\n")
+    note = input('Enter note: ')
+    with open('notes.ntui', 'a') as f:
+        f.write(note + '\n')
+    print('Note saved.')
 
 def delete():
-    num = int(input())
-    with open("notes.nt", "r") as f:
-        notes = f.readlines()
-    if 1 <= num <= len(notes):
-        del notes[num-1]
-        with open("notes.nt", "w") as f:
-            f.writelines(notes)
+    view()
+    try:
+        num = int(input('Note to delete: '))
+        with open('notes.ntui', 'r') as f:
+            notes = f.readlines()
+        if 1 <= num <= len(notes):
+            del notes[num-1]
+            with open('notes.ntui', 'w') as f:
+                f.writelines(notes)
+            print('Note deleted.')
+        else:
+            print('err: invalid number')
+    except:
+        print('err: unknown')
 
 def search():
-    find = input()
-    with open("notes.nt", "r") as f:
+    s = input('Search: ')
+    with open('notes.ntui', 'r') as f:
         notes = f.readlines()
-    for i, n in enumerate(notes):
-        if find in n:
-            print(f"{i}. {n.strip()}")
+    for i, n in enumerate(notes, 1):
+        if s in n:
+            print(f'{i}. {n.strip()}')
 
 def close():
+    print('Program terminated, have a nice day!' + '\n' + '>_<')
     exit()
 
 def view():
-    with open("notes.nt", "r") as f:
+    with open('notes.ntui', 'r') as f:
         notes = f.readlines()
-    for i, n in enumerate(notes, 1):
-        print(f"{i}. {n.strip()}")
+    for i, note in enumerate(notes, 1):
+        print(f'{i}. {note.strip()}')
 
-with open("notes.nt", "w") as init:
-    ...
+print('\n' + "'interafce()' by Chevy-SS-1970 on github" + '\n')
+print('Greetings, my Lord! We praise thee.')
 interface()
